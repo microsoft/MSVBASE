9464834ca5043b268e5a1fcd27ffe681aa3dbcf1
Qianxi Zhang <qiazh@microsoft.com>
2023-06-19 07:02:02 +0000

    use gcc 12 docker file

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -2,4 +2,4 @@
 #VERSION=$(git rev-parse --short HEAD)
 #DOCKERNAME=vectordb-$VERSION
 
-docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t vbase_artifacts -f Dockerfile .
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t vbase_open_source -f Dockerfile .
--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CWD=$(pwd)
-docker run --name=vbase_artifacts --security-opt seccomp=seccomp-perf.json -v /dev/shm/artifacts:/u02  -v /datadrive/vbase_artifacts:/artifacts -e PGPASSWORD=vectordb -e PGUSERNAME=vectordb -e PGDATABASE=vectordb -v $CWD:/vectordb vbase_artifacts &
+docker run --name=vbase_open_source  -v /datadrive/vbase_artifacts:/artifacts -e PGPASSWORD=vectordb -e PGUSERNAME=vectordb -e PGDATABASE=vectordb -v $CWD:/vectordb vbase_open_source &
 sleep 20
 str=$"\n"
 sstr=$(echo -e $str)
